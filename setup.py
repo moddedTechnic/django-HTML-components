36d8cee9e5259dc596f7d9cb9d6632ae7747fa82
moddedTechnic <jpleeming51@gmail.com>
2020-01-23 19:59:07 +0000

    Added PyPI files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+	name='django_components',
+	packages=['django_components'],
+	version='0.1',
+	license='GNU GPL 3.0',
+	description='Adds several commonly used components for Django, such as a styled header and footer.',
+	author='Jonathan Leeming',
+	author_email='jpleeming51@gmail.com',
+	url='https://github.com/moddedTechnic/django-components',
+	download_url='', #TODO: Fill in download url
+	keywords=['django', 'HTML', 'components'],
+	install_requires=[], #TODO: Fill in requirements
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development',
+		'License :: OSI Approved :: GNU GPL 3.0',
+		'Programming Language :: Python ::  3',
+		'Programming Language :: Python ::  3.8',
+	],
+)
