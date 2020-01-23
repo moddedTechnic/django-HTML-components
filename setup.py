20c5140fb40c0307371dc12ca08242e36f8870ba
moddedTechnic <jpleeming51@gmail.com>
2020-01-23 20:10:13 +0000

    Adjusted config

--- setup.py
+++ setup.py
@@ -9,14 +9,17 @@ setup(
 	author='Jonathan Leeming',
 	author_email='jpleeming51@gmail.com',
 	url='https://github.com/moddedTechnic/django-components',
-	download_url='', #TODO: Fill in download url
+	download_url='https://github.com/moddedTechnic/django-components/archive/0.1.tar.gz',
 	keywords=['django', 'HTML', 'components'],
-	install_requires=[], #TODO: Fill in requirements
+	install_requires=[
+		'django',
+	],
 	classifiers=[
 		'Development Status :: 3 - Alpha',
 		'Intended Audience :: Developers',
+		'Framework :: Django :: 3.0',
 		'Topic :: Software Development',
-		'License :: OSI Approved :: GNU GPL 3.0',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Programming Language :: Python ::  3',
 		'Programming Language :: Python ::  3.8',
 	],
