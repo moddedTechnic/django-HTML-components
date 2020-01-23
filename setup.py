0532d5b3ac718feb6ca6c141160045ffe1ded488
moddedTechnic <jpleeming51@gmail.com>
2020-01-23 20:17:11 +0000

    Modified settings to hopefully work with PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-	name='django_components',
-	packages=['django_components'],
+	name='django_HTML_components',
+	packages=['django_HTML_components'],
 	version='0.1',
 	license='GNU GPL 3.0',
 	description='Adds several commonly used components for Django, such as a styled header and footer.',
 	author='Jonathan Leeming',
 	author_email='jpleeming51@gmail.com',
-	url='https://github.com/moddedTechnic/django-components',
-	download_url='https://github.com/moddedTechnic/django-components/archive/0.1.tar.gz',
+	url='https://github.com/moddedTechnic/django-HTML-components',
+	download_url='https://github.com/moddedTechnic/django-HTML-components/archive/v0.1.1-alpha.tar.gz',
 	keywords=['django', 'HTML', 'components'],
 	install_requires=[
 		'django',
@@ -20,7 +20,7 @@ setup(
 		'Framework :: Django :: 3.0',
 		'Topic :: Software Development',
 		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Programming Language :: Python ::  3',
-		'Programming Language :: Python ::  3.8',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.8',
 	],
 )
