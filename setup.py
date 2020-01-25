991077fef7120ec62a83a5c74c54dfc4ebff3ec0
moddedTechnic <jpleeming51@gmail.com>
2020-01-25 08:38:26 +0000

    Update 0.3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
 	name='django_HTML_components',
 	packages=['django_HTML_components'],
-	version='0.2',
+	version='0.3',
 	license='GNU GPL 3.0',
 	description='Adds several commonly used components for Django, such as a styled header and footer.',
 	author='Jonathan Leeming',
 	author_email='jpleeming51@gmail.com',
 	url='https://github.com/moddedTechnic/django-HTML-components',
-	download_url='https://github.com/moddedTechnic/django-HTML-components/archive/v0.1.4-alpha.tar.gz',
+	download_url='https://github.com/moddedTechnic/django-HTML-components/archive/v0.1.5-alpha.tar.gz',
 	keywords=['django', 'HTML', 'components'],
 	install_requires=[
 		'django',
